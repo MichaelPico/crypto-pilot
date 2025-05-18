9e2337b1bbc9504ae4e0de3214215173f226d18a
Michael <michael.pico.sandoval@gmail.com>
2025-05-18 22:14:34 +0200

    Add checks for schema and table existence before creation in create-tables.sql

--- Crypto.Pilot.Database/create-tables.sql
+++ Crypto.Pilot.Database/create-tables.sql
@@ -1,28 +1,42 @@
-CREATE TABLE [crypto_pilot].[users] (
-    [id] INT NOT NULL IDENTITY(1, 1),
-    [name] NVARCHAR(100) NOT NULL,
-    [email] NVARCHAR(100) NOT NULL UNIQUE,
-    [phone_number] NVARCHAR(20) NOT NULL,
-    CONSTRAINT [PK_users] PRIMARY KEY ([id])
-);
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'crypto_pilot')
+    EXEC('CREATE SCHEMA crypto_pilot');
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'pilot')
+    EXEC('CREATE SCHEMA pilot');
+
+IF NOT EXISTS (SELECT * FROM sys.tables t JOIN sys.schemas s ON t.schema_id = s.schema_id WHERE t.name = 'users' AND s.name = 'crypto_pilot')
+BEGIN
+    CREATE TABLE [crypto_pilot].[users] (
+        [id] INT NOT NULL IDENTITY(1, 1),
+        [name] NVARCHAR(100) NOT NULL,
+        [email] NVARCHAR(100) NOT NULL UNIQUE,
+        [phone_number] NVARCHAR(20) NOT NULL,
+        CONSTRAINT [PK_users] PRIMARY KEY ([id])
+    );
+END
 GO
 
-CREATE TABLE [pilot].[cryptocurrencies] (
-    [id] INT NOT NULL IDENTITY(1, 1),
-    [name] NVARCHAR(100) NOT NULL,
-    [current_price] FLOAT NOT NULL,
-    CONSTRAINT [PK_cryptocurrencies] PRIMARY KEY ([id])
-);
+IF NOT EXISTS (SELECT * FROM sys.tables t JOIN sys.schemas s ON t.schema_id = s.schema_id WHERE t.name = 'cryptocurrencies' AND s.name = 'pilot')
+BEGIN
+    CREATE TABLE [pilot].[cryptocurrencies] (
+        [id] INT NOT NULL IDENTITY(1, 1),
+        [name] NVARCHAR(100) NOT NULL,
+        [current_price] FLOAT NOT NULL,
+        CONSTRAINT [PK_cryptocurrencies] PRIMARY KEY ([id])
+    );
+END
 GO
 
-CREATE TABLE [pilot].[alerts] (
-    [id] INT NOT NULL IDENTITY(1, 1),
-    [user_id] INT NOT NULL,
-    [cryptocurrency_id] INT NOT NULL,
-    [target_price] FLOAT NOT NULL,
-    [notified] BIT NOT NULL DEFAULT 0,
-    CONSTRAINT [PK_alerts] PRIMARY KEY ([id]),
-    CONSTRAINT [FK_alerts_users] FOREIGN KEY ([user_id]) REFERENCES [crypto].[users]([id]),
-    CONSTRAINT [FK_alerts_cryptocurrencies] FOREIGN KEY ([cryptocurrency_id]) REFERENCES [crypto].[cryptocurrencies]([id])
-);
+IF NOT EXISTS (SELECT * FROM sys.tables t JOIN sys.schemas s ON t.schema_id = s.schema_id WHERE t.name = 'alerts' AND s.name = 'pilot')
+BEGIN
+    CREATE TABLE [pilot].[alerts] (
+        [id] INT NOT NULL IDENTITY(1, 1),
+        [user_id] INT NOT NULL,
+        [cryptocurrency_id] INT NOT NULL,
+        [target_price] FLOAT NOT NULL,
+        [notified] BIT NOT NULL DEFAULT 0,
+        CONSTRAINT [PK_alerts] PRIMARY KEY ([id]),
+        CONSTRAINT [FK_alerts_users] FOREIGN KEY ([user_id]) REFERENCES [crypto].[users]([id]),
+        CONSTRAINT [FK_alerts_cryptocurrencies] FOREIGN KEY ([cryptocurrency_id]) REFERENCES [crypto].[cryptocurrencies]([id])
+    );
+END
 GO
