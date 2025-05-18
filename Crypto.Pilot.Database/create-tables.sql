dd427e5cd3c0be066de187f79ad76d935c6c7b59
Michael <michael.pico.sandoval@gmail.com>
2025-05-18 22:56:23 +0200

    Fix schema references in create-tables.sql to ensure correct table creation under 'crypto_pilot'

--- Crypto.Pilot.Database/create-tables.sql
+++ Crypto.Pilot.Database/create-tables.sql
@@ -1,7 +1,5 @@
 IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'crypto_pilot')
     EXEC('CREATE SCHEMA crypto_pilot');
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'pilot')
-    EXEC('CREATE SCHEMA pilot');
 
 IF NOT EXISTS (SELECT * FROM sys.tables t JOIN sys.schemas s ON t.schema_id = s.schema_id WHERE t.name = 'users' AND s.name = 'crypto_pilot')
 BEGIN
@@ -15,9 +13,9 @@ BEGIN
 END
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.tables t JOIN sys.schemas s ON t.schema_id = s.schema_id WHERE t.name = 'cryptocurrencies' AND s.name = 'pilot')
+IF NOT EXISTS (SELECT * FROM sys.tables t JOIN sys.schemas s ON t.schema_id = s.schema_id WHERE t.name = 'cryptocurrencies' AND s.name = 'crypto_pilot')
 BEGIN
-    CREATE TABLE [pilot].[cryptocurrencies] (
+    CREATE TABLE [crypto_pilot].[cryptocurrencies] (
         [id] INT NOT NULL IDENTITY(1, 1),
         [name] NVARCHAR(100) NOT NULL,
         [current_price] FLOAT NOT NULL,
@@ -26,17 +24,17 @@ BEGIN
 END
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.tables t JOIN sys.schemas s ON t.schema_id = s.schema_id WHERE t.name = 'alerts' AND s.name = 'pilot')
+IF NOT EXISTS (SELECT * FROM sys.tables t JOIN sys.schemas s ON t.schema_id = s.schema_id WHERE t.name = 'alerts' AND s.name = 'crypto_pilot')
 BEGIN
-    CREATE TABLE [pilot].[alerts] (
+    CREATE TABLE [crypto_pilot].[alerts] (
         [id] INT NOT NULL IDENTITY(1, 1),
         [user_id] INT NOT NULL,
         [cryptocurrency_id] INT NOT NULL,
         [target_price] FLOAT NOT NULL,
         [notified] BIT NOT NULL DEFAULT 0,
         CONSTRAINT [PK_alerts] PRIMARY KEY ([id]),
-        CONSTRAINT [FK_alerts_users] FOREIGN KEY ([user_id]) REFERENCES [crypto].[users]([id]),
-        CONSTRAINT [FK_alerts_cryptocurrencies] FOREIGN KEY ([cryptocurrency_id]) REFERENCES [crypto].[cryptocurrencies]([id])
+        CONSTRAINT [FK_alerts_users] FOREIGN KEY ([user_id]) REFERENCES [crypto_pilot].[users]([id]),
+        CONSTRAINT [FK_alerts_cryptocurrencies] FOREIGN KEY ([cryptocurrency_id]) REFERENCES [crypto_pilot].[cryptocurrencies]([id])
     );
 END
 GO
